a42241c448977c99a26094addef6bb93059f3527
Sergio <sergio.pena@abiquo.com>
2016-12-05 18:23:16 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kekio.one@gmail.com'
 license          'Apache 2.0'
 description      'Compile and installs netdata'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.7'
+version          '0.1.8'
 source_url		 'https://github.com/sergiopena/netdata-cookbook'
 issues_url		 'https://github.com/sergiopena/netdata-cookbook/issues'
 
