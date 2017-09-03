cd3a29cdc0acd11c48daae5436342f56a762e641
nictrix <nickwillever@gmail.com>
2017-09-03 14:04:42 -0700

    add sleep, to allow NetData to startup

--- test/integration/general/default_spec.rb
+++ test/integration/general/default_spec.rb
@@ -13,7 +13,8 @@ describe service('netdata') do
   it { should be_running }
 end
 
-describe command("curl -s 'http://localhost:19999/api/v1/allmetrics'") do
+describe command(
+  "sleep 5; curl -s 'http://localhost:19999/api/v1/allmetrics'") do
   its('stdout') { should match(/NETDATA_SYSTEM_CPU_SYSTEM=.*/) }
 end
 
