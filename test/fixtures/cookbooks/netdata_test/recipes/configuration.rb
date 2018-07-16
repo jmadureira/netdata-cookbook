b75620872f3310d5a484e8b92187d4c464a20272
João Madureira <jmadureira@invisiblecloud.pt>
2018-07-16 22:33:28 +0100

    Removed bind_rndc_conf resource

--- test/fixtures/cookbooks/netdata_test/recipes/configuration.rb
+++ test/fixtures/cookbooks/netdata_test/recipes/configuration.rb
@@ -79,8 +79,3 @@ end
 
 node.override['netdata']['plugins']['python']['bind_rndc']['config'] \
   ['named_stats_path'] = '/custom/path'
-
-netdata_bind_rndc_conf 'default_bind_rndc_config' do
-  named_stats_path node['netdata']['plugins']['python'] \
-    ['bind_rndc']['config']['named_stats_path']
-end
